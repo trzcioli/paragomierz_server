b31c0abe13e099c953e6cb63a6cbf8cead03a07a
Oliwia Trzcinska <trzcioli@gmail.com>
2020-06-24 22:33:14 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, Response, jsonify, make_response
+import numpy as np
+import cv2
+import projekt
+import json
+from dataclasses_serialization.json import JSONSerializer
+
+app = Flask(__name__)
+
+
+@app.route('/api/test', methods=['POST'])
+def test():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+
+    # do processing here....
+    res = projekt.main(img)
+    print(res)
+    js = []
+
+    for i in res:
+        js.append(JSONSerializer.serialize(i))
+    # build a response dict to send back to client
+    return Response(json.dumps(js, ensure_ascii=False).encode('utf8'),  mimetype='application/json')
+
+
+# start flask app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
